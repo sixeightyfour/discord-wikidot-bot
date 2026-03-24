27b5ebca6092f76a219328e08363a53c5d91a459
sixeightyfour <scpthrowawayfour@proton.me>
2026-03-24 07:00:12 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,9 +14,9 @@ except ImportError:
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID", "0"))
 
-CHECK_URL = "https://this-does-not-exist-12345.com/"
-CHECK_INTERVAL_SECONDS = 10
-FAIL_THRESHOLD = 1
+CHECK_URL = "https://scp-wiki.wikidot.com/"
+CHECK_INTERVAL_SECONDS = 60
+FAIL_THRESHOLD = 3
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
